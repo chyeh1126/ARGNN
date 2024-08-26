e0bdfe6936c58b78ea4e15b6df8fea67079bbf45
chyeh1126 <61863627+chyeh1126@users.noreply.github.com>
2024-08-26 19:46:34 +0800

    Add files via upload

---
+++ experiments/citeseer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+for seed in {10..14}
+do
+    python train_ARGNN_feature.py \
+        --seed ${seed} \
+        --dataset citeseer \
+        --attack_structure no \
+        --attack_feature no \
+        --ptb_rate 0.15 \
+        --alpha 3 \
+        --sigma 100 \
+        --beta 0.3 \
+        --eta 1 \
+        --t_small 0.1 \
+        --lr 1e-3 \
+        --lr_adj 1e-3 \
+        --epoch 1000 \
+        --label_rate 0.01 \
+        --threshold 0.8 \
+        --n_p 400 \
+        --gnnlayers 3 \
+        --linlayers 1 \
+        --upth_st 0.0015 \
+        --upth_ed 0.001 \
+        --lowth_st 0.1 \
+        --lowth_ed 0.5 \
+        --val_rate 0.1 \
+        --test_rate 0.8
+done
